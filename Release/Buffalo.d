3d58b5a14e4c2725a9f5b782ec3361028c209da2
Russ White <commits@twistedpearaudio.com>
2018-04-24 14:46:44 -0500

    Add binaries

---
+++ Release/Buffalo.d
@@ -0,0 +1,10 @@
+Buffalo.o: ../Buffalo.c ../USI_I2C_Master.h ../Buffalo.h ../ES9028_38.h \
+ ../I2C_CharDisplay.h
+
+../USI_I2C_Master.h:
+
+../Buffalo.h:
+
+../ES9028_38.h:
+
+../I2C_CharDisplay.h:
---
+++ Release/I2C_CharDisplay.d
@@ -0,0 +1,6 @@
+I2C_CharDisplay.o: ../I2C_CharDisplay.c ../USI_I2C_Master.h \
+ ../I2C_CharDisplay.h
+
+../USI_I2C_Master.h:
+
+../I2C_CharDisplay.h:
